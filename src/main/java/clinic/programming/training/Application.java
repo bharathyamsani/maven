3021390e054ebd62b30407d9bc362203f3ce260b
bharath <yamsani.bharath@effigo.tech>
2024-01-18 14:08:31 +0530

    Change compiler plugin

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,23 @@
 package clinic.programming.training;
 
+import java.util.*;
+
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public void greet(){
+		List<String> g=new ArrayList<>();
+		g.add("Hello");
+		for(String s:g) System.out.println(s);
+	}
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
-	Application app = new Application();
+		Application app = new Application();
+		app.greet();
     }
 }
