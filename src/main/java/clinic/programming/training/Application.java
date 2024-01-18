0da4a569b149aa2cd1ce332b2f414b1234ac28ef
bharath <yamsani.bharath@effigo.tech>
2024-01-18 16:05:19 +0530

    Add dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,12 +1,18 @@
 package clinic.programming.training;
 
 import java.util.*;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public int countWords(String s){
+		String []words=StringUtils.split(s," ");
+		return words.length;
+	}
 	
 	public void greet(){
 		List<String> g=new ArrayList<>();
@@ -16,8 +22,11 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
     	System.out.println ("Starting Application");
 		Application app = new Application();
 		app.greet();
+		System.out.println("Enter a sentence:");
+		System.out.println("No.of words="+app.countWords(sc.nextLine()));
     }
 }
